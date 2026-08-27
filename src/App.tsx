55d5ee2ea941d12262025912c7363db9b0e8c00b
fmSand <fridamsand@gmail.com>
2026-08-27 20:46:49 +0200

    feat(i18n): wire language toggle into app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useLayoutEffect } from 'react';
 import { Route, Routes } from 'react-router';
-import Home from './pages/Home';
+import { LanguageToggle } from '@/components/LanguageToggle';
+import { useLanguageStore } from '@/stores/languageStore';
+import Home from '@/pages/Home';
 
 function App() {
+  const language = useLanguageStore((state) => state.language);
+
+  // TODO: Stands in for shared Layout's effect until #4/#7 get implemented
+  useLayoutEffect(() => {
+    document.documentElement.lang = language;
+  }, [language]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      {/* Temporary langtoggle here, movee into footer with #8. */}
+      <LanguageToggle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
