e7ca8219096c537fe55168e11ce0b095d45f20e0
Frida Sand <163577510+fmSand@users.noreply.github.com>
2026-09-21 18:29:41 +0200

    chore: remove temporary button from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,6 @@ function Home() {
     <section className="bg-background">
       <h1>Home</h1>
       <p>yello.</p>
-      {/* TEMP: Secondary button preview for fix-button-tokens. NB! Leaving it just for review*/}
-      <button type="button" className="btn btn-outline mt-5">
-        Download Resume
-        <ChevronDown aria-hidden="true" />
-      </button>
     </section>
   );
 }
