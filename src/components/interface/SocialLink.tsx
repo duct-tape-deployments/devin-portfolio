5564565542574bb3cfdb01353cd6ecef9d50d48a
fmSand <fridamsand@gmail.com>
2026-09-26 11:48:39 +0200

    fix(styles): match social link pressed state to Figma with 2px shrink and default shadow

--- src/components/interface/SocialLink.tsx
+++ src/components/interface/SocialLink.tsx
@@ -18,7 +18,7 @@ export function SocialLink({ label, href, icon: Icon, disabled = false }: Social
       tabIndex={disabled ? -1 : undefined}
       className="social-link"
     >
-      <Icon aria-hidden="true" className="size-6" />
+      <Icon aria-hidden="true" size={24} />
     </a>
   );
 }
